9e71633e5b8ae3f25ec3fd1ab959e0601a151c4e
Sergey Filippenkov <fsvmxd@yandex.ru>
2023-04-17 01:10:52 +0300

    poject Java updatr

--- Java/AccountingForTheNursery/Main.java
+++ Java/AccountingForTheNursery/Main.java
@@ -1,35 +1,39 @@
-import ConstructAnimals.*;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     UserInterface ui = new UserInterface();
-    Modul_db md = new Modul_db();
-    private Map<String, String> dataAnimals;
+    Module_db md = new Module_db();
+    /**
+     * Массив данных о животном
+     * (dataAnimals[0] - nickname, dataAnimals[1] - breed, dataAnimals[2] - dateOfBirth)
+     */
+    private String[] dataAnimals;
 
     public static void main(String[] args) {
         Main main = new Main();
         main.runMainMenu();
     }
 
+    /**
+     * Обработка главного меню
+     */
     public void runMainMenu() {
         ui.showMenu(ui.mainMenu());
         while (true) {
-            switch (ui.inputUserMenu(ui.mainMenu())) {
+            int numberMenu = ui.inputUserMenu(ui.mainMenu());
+            switch (numberMenu) {
                 case 1 -> {
                     System.out.println("\nВыберите раздел для создания");
                     runCreateAnimalsMenu();
                 }
                 case 2 -> {
                     System.out.println("\nВыберите раздел для чтения");
-                    runAnimalsMenu();
+                    runAnimalsMenu(numberMenu);
                 }
                 case 3 -> {
-                    System.out.println("\nВыберите раздел для редактирования");
-                    runAnimalsMenu();
+                    System.out.println("\nВыберите раздел для добавления команды");
+                    runAnimalsMenu(numberMenu);
                 }
                 case 4 -> {
                     System.out.println("Завершение программы...");
@@ -40,75 +44,54 @@ public class Main {
         }
     }
 
+    /**
+     * Метод обработки записи данных в справочник
+     */
     public void runCreateAnimalsMenu() {
-        ui.showMenu(ui.animalsMenuCreate());
         while (true) {
+            ui.showMenu(ui.animalsMenuCreate());
             switch (ui.inputUserMenu(ui.animalsMenuCreate())) {
                 case 1 -> {
-                    System.out.println("Введите данные Собаки");
+                    String name = "dogs";
+                    System.out.println("Введите данные о собаке");
                     dataAnimals = ui.inputDataAnimals();
-                    Dogs dogs = new Dogs(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные о собаке внесены");
-                    md.createFile("dogs", dogs.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 2 -> {
-                    System.out.println("Введите данные Кошки");
+                    String name = "cats";
+                    System.out.println("Введите данные о кошке");
                     dataAnimals = ui.inputDataAnimals();
-                    Cats cats = new Cats(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные о кошке внесены");
-                    md.createFile("cats", cats.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 3 -> {
-                    System.out.println("Введите данные Хомяка");
+                    String name = "hamsters";
+                    System.out.println("Введите данные о хомяке");
                     dataAnimals = ui.inputDataAnimals();
-                    Hamsters hamsters = new Hamsters(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные о хомяке внесены");
-                    md.createFile("hamsters", hamsters.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 4 -> {
-                    System.out.println("Введите данные Лошади");
+                    String name = "horses";
+                    System.out.println("Введите данные о лошади");
                     dataAnimals = ui.inputDataAnimals();
-                    Horses horses = new Horses(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные о лошади внесены");
-                    md.createFile("horses", horses.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 5 -> {
-                    System.out.println("Введите данные Верблюда");
+                    String name = "camels";
+                    System.out.println("Введите данные о верблюде");
                     dataAnimals = ui.inputDataAnimals();
-                    Camels camels = new Camels(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные о верблюде внесены");
-                    md.createFile("camels", camels.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 6 -> {
-                    System.out.println("Введите данные Осла");
+                    String name = "donkeys";
+                    System.out.println("Введите данные об осле");
                     dataAnimals = ui.inputDataAnimals();
-                    Donkeys donkeys = new Donkeys(
-                            dataAnimals.get("nickname"),
-                            dataAnimals.get("breed"),
-                            LocalDate.parse(dataAnimals.get("dateOfBirth")));
                     System.out.println("Данные об осле внесены");
-                    md.createFile("donkeys", donkeys.toString());
-                    ui.showMenu(ui.animalsMenuCreate());
+                    md.createFile(name, dataAnimals);
                 }
                 case 7 -> {
                     ui.showMenu(ui.mainMenu());
@@ -119,42 +102,78 @@ public class Main {
         }
     }
 
-    public void runAnimalsMenu() {
+    public void runAnimalsMenu(int numberMenu) {
         ui.showMenu(ui.animalsMenu());
         while (true) {
             switch (ui.inputUserMenu(ui.animalsMenu())) {
                 case 1 -> {
-                    System.out.println("Данные о Собаках");
-                    md.readeFile("dogs");
-                    ui.showMenu(ui.animalsMenu());
+                    String name = "dogs";
+                    System.out.println("Данные о Собаках\n");
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 2 -> {
-                    System.out.println("Данные о Кошках");
-                    md.readeFile("cats");
-                    ui.showMenu(ui.animalsMenu());
+                    System.out.println("Данные о Кошках\n");
+                    String name = "cats";
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 3 -> {
-                    System.out.println("Данные о Хомяках");
-                    md.readeFile("hamsters");
-                    ui.showMenu(ui.animalsMenu());
+                    System.out.println("Данные о Хомяках\n");
+                    String name = "hamsters";
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 4 -> {
-                    System.out.println("Данные о Лошадях");
-                    md.readeFile("horses");
-                    ui.showMenu(ui.animalsMenu());
+                    System.out.println("Данные о Лошадях\n");
+                    String name = "horses";
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 5 -> {
-                    System.out.println("Данные о Верблюдах");
-                    md.readeFile("camels");
-                    ui.showMenu(ui.animalsMenu());
+                    System.out.println("Данные о Верблюдах\n");
+                    String name = "camels";
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 6 -> {
-                    System.out.println("Данные о Ослах");
-                    md.readeFile("donkeys");
-                    ui.showMenu(ui.animalsMenu());
+                    System.out.println("Данные о Ослах\n");
+                    String name = "donkeys";
+                    if (numberMenu == 3 && md.thereIsFile(name)) {
+                        //TODO: Реализовать внесение изученных команд
+                    } else {
+                        ui.outputContentsDirectory(md.readeFile(name));
+                        System.out.println();
+                        ui.showMenu(ui.animalsMenu());
+                    }
                 }
                 case 7 -> {
-                    System.out.println("Данные о всех животных");
+                    System.out.println("Данные о всех животных\n");
                     List<String> nameFile = new ArrayList<>();
                     nameFile.add("dogs");
                     nameFile.add("cats");
@@ -163,8 +182,9 @@ public class Main {
                     nameFile.add("camels");
                     nameFile.add("donkeys");
                     for (String name : nameFile) {
-                        md.readeFileAll(name);
+                        ui.outputContentsDirectory(md.readeFile(name));
                     }
+                    System.out.println();
                     ui.showMenu(ui.animalsMenu());
                 }
                 case 8 -> {
--- Java/AccountingForTheNursery/UserInterface.java
+++ Java/AccountingForTheNursery/UserInterface.java
@@ -1,8 +1,16 @@
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
 
+/**
+ * Класс взаимодействия с пользователем
+ */
 public class UserInterface {
-
+    /**
+     * Вывод в консоль пунктов меню
+     */
     public void showMenu(List<String> menu) {
         int i = 0;
         for (String menuItem : menu) {
@@ -11,16 +19,53 @@ public class UserInterface {
         }
     }
 
+    /**
+     * Список главного меню
+     */
     public List<String> mainMenu() {
         List<String> mainMenu = new ArrayList<>();
         mainMenu.add("Создать запись");
         mainMenu.add("Прочитать записи");
-        mainMenu.add("Редактировать записи");
+        mainMenu.add("Выполняемые команды");
         mainMenu.add("Выход");
 
         return mainMenu;
     }
 
+    /**
+     * Список каталогов при внесении в базу
+     */
+    public List<String> animalsMenuCreate() {
+        List<String> animalsMenu = new ArrayList<>();
+        animalsMenu.add("Собаки");
+        animalsMenu.add("Кошки");
+        animalsMenu.add("Хомяки");
+        animalsMenu.add("Лошади");
+        animalsMenu.add("Верблюды");
+        animalsMenu.add("Ослы");
+        animalsMenu.add("НАЗАД");
+        return animalsMenu;
+    }
+
+    /**
+     * Вывод каталогов при всех остальных пунктах главного меню
+     */
+    public List<String> animalsMenu() {
+        List<String> animalsMenu = new ArrayList<>();
+        animalsMenu.add("Собаки");
+        animalsMenu.add("Кошки");
+        animalsMenu.add("Хомяки");
+        animalsMenu.add("Лошади");
+        animalsMenu.add("Верблюды");
+        animalsMenu.add("Ослы");
+        animalsMenu.add("Все разделы");
+        animalsMenu.add("НАЗАД");
+        return animalsMenu;
+    }
+
+    /**
+     * Метод обработки выбора пунктов меню
+     */
     public int inputUserMenu(List<String> menu) {
         try {
             Scanner input = new Scanner(System.in);
@@ -35,38 +80,16 @@ public class UserInterface {
         return 0;
     }
 
-    public List<String> animalsMenuCreate() {
-        List<String> animalsMenu = new ArrayList<>();
-        animalsMenu.add("Собаки");
-        animalsMenu.add("Кошки");
-        animalsMenu.add("Хомяки");
-        animalsMenu.add("Лошади");
-        animalsMenu.add("Верблюды");
-        animalsMenu.add("Ослы");
-        animalsMenu.add("НАЗАД");
-        return animalsMenu;
-    }
-
-    public List<String> animalsMenu() {
-        List<String> animalsMenu = new ArrayList<>();
-        animalsMenu.add("Собаки");
-        animalsMenu.add("Кошки");
-        animalsMenu.add("Хомяки");
-        animalsMenu.add("Лошади");
-        animalsMenu.add("Верблюды");
-        animalsMenu.add("Ослы");
-        animalsMenu.add("Все разделы");
-        animalsMenu.add("НАЗАД");
-        return animalsMenu;
-    }
-
-    public Map<String, String> inputDataAnimals() {
-        Map<String, String> dataAnimals = new HashMap<>();
+    /**
+     * Метод обработки ввода данных в базу
+     */
+    public String[] inputDataAnimals() {
+        String[] dataAnimals = new String[3];
         Scanner input = new Scanner(System.in);
         System.out.print("Введите кличку: ");
-        dataAnimals.put("nickname", input.nextLine());
+        dataAnimals[0] = input.nextLine();
         System.out.print("Введите породу: ");
-        dataAnimals.put("breed", input.nextLine());
+        dataAnimals[1] = input.nextLine();
         int dey = 0;
         int month = 0;
         int year = 0;
@@ -78,11 +101,22 @@ public class UserInterface {
                 month = input.nextInt();
                 System.out.print("Введите год рождения: ");
                 year = input.nextInt();
-                dataAnimals.put("dateOfBirth", LocalDate.of(year, month, dey).toString());
+                dataAnimals[2] = LocalDate.of(year, month, dey).toString();
             } catch (InputMismatchException e) {
                 System.out.println("Вы ввели не цифру");
             }
             return dataAnimals;
+        }
+    }
+
+    /**
+     * Метод вывода содержимого справочника в консоль
+     */
+    public void outputContentsDirectory(List<String[]> dataDirectory) {
+        for (String[] line : dataDirectory) {
+            if (line.length == 3) {
+                System.out.println(line[0] + " " + line[1] + " " + line[2]);
+            }
         }
     }
 }
